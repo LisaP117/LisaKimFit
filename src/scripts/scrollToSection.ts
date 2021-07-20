cbcfc299e46af40cf262d22c2c482b30f439689a
Lisa Patel <lisa.patel@coopdigital.co.uk>
2021-07-20 12:55:20 +0100

    feat(amends): updates to navigation and functionality

--- src/scripts/scrollToSection.ts
+++ src/scripts/scrollToSection.ts
@@ -1,8 +1,9 @@
 const scrollTo = (links: HTMLElement) => {
 
 	const scrollTo = (e: Event) => {
-		const elem = e.target as HTMLElement;
-		const section = document.querySelector("#" + elem.dataset.link);
+		const elem = e.target as HTMLAnchorElement;
+		const elemTarget = elem.getAttribute('href');
+		const section = document.querySelector(`${elemTarget}`) as HTMLElement;
 		
 		if (section !== null ){
 			section.scrollIntoView({
@@ -10,7 +11,13 @@ const scrollTo = (links: HTMLElement) => {
 				block: "start",
 				inline: "nearest"
 			});
+
+			setTimeout(() => {
+				window.location.hash = `${elemTarget}`;
+			}, 700);
 		}
+
+		e.preventDefault();
 
 	}
 
